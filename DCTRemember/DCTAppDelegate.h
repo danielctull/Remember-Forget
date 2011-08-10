0a778035db7323bcdc5729f93ab83b34c3c7d432
Daniel Tull <dt@danieltull.co.uk>
2011-08-10 11:54:58 +0100

    Initial Commit

---
+++ DCTRemember/DCTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCTAppDelegate.h
+//  DCTRemember
+//
+//  Created by Daniel Tull on 10.08.2011.
+//  Copyright (c) 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DCTRemember/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DCTRemember
+//
+//  Created by Daniel Tull on 10.08.2011.
+//  Copyright (c) 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DCTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DCTAppDelegate class]));
+	}
+}
